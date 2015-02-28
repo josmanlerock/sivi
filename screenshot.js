31750ef5451a3236a5c559a92fb6fdc518812554
Hubber <hubber@MacBook-Pro-de-TelmexHub-4.local>
2015-02-28 00:40:34 -0600

    aun no podemos guardar la imagen

--- screenshot.js
+++ screenshot.js
@@ -5,3 +5,15 @@
 function setScreenshotUrl(url) {
   document.getElementById('target').src = url;
 }
+
+       window.onload = function () {
+                var img = document.getElementById('target');
+                var button = document.getElementById('saveImage');
+                //img.src = 'white.png';
+                img.onload = function () {
+                    button.removeAttribute('disabled');
+                };
+                button.onclick = function () {
+                    window.location.href = img.src.replace('image/png', 'image/octet-stream');
+                };
+            };
